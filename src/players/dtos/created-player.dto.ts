efbdeef9cd83e523a69ae018d37ce2c8aad11f57
Sidicley Schneider <sidi.fev@gmail.com>
2021-08-23 22:39:54 -0300

    player controller(interface-dtos)

---
+++ src/players/dtos/created-player.dto.ts
@@ -0,0 +1,5 @@
+export class CreatedPlayerDto {
+  readonly phoneNumber: string;
+  readonly email: string;
+  readonly name: string;
+}
--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatedPlayerDto } from './dtos/created-player.dto';
 
-@Controller('players')
-export class PlayersController {}
+@Controller('api/v1/players')
+export class PlayersController {
+  @Post()
+  async createUpdatePlayer(@Body() createdPlayerDto: CreatedPlayerDto) {
+    const { email, name } = createdPlayerDto;
+    return JSON.stringify(`{
+      email: ${email}
+      name: ${name}
+    }`);
+  }
+}
